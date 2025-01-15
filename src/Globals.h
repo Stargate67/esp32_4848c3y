fa328963fa8e77e1720d279907490e74921484fa
Stargate67 <66971848+Stargate67@users.noreply.github.com>
2025-01-15 23:35:24 +0100

    Step11

--- src/Globals.h
+++ src/Globals.h
@@ -82,42 +82,42 @@ lv_obj_t *scr;
 
 constexpr uint16_t MB_READ_INTERVAL = 1000; 
 
-constexpr uint16_t START_REG = 12688;       // Starting holding register
-constexpr uint16_t NB_REGS = 11;           // Number of holding registers to read
+constexpr uint16_t START_REG = 12688;            // Starting holding register
+constexpr uint16_t NB_REGS = 11;                // Number of holding registers to read
 
 
-constexpr uint16_t MASK_CHAUD = 0x1; // Bit 0
-constexpr uint16_t MASK_BOOST_ANIM = 0x4000; // Bit 14  Pourretour etat BOOST
-constexpr uint16_t MASK_BOOST_MA = 0x10; // Bit 5   Pour Commande Marche Arret
+constexpr uint16_t MASK_CHAUD = 0x1;            // Bit 0
+constexpr uint16_t MASK_BOOST_ANIM = 0x4000;    // Bit 14  Pourretour etat BOOST
+constexpr uint16_t MASK_BOOST_MA = 0x10;        // Bit 5   Pour Commande Marche Arret
 
 
-constexpr uint16_t MASK_PPERADIAT = 0x2; // Bit 1
-constexpr uint16_t MASK_PPEPLANCHER = 0x4; // Bit 2
+constexpr uint16_t MASK_PPERADIAT = 0x2;        // Bit 1
+constexpr uint16_t MASK_PPEPLANCHER = 0x4;      // Bit 2
 
 //constexpr uint16_t START_REG_BP_MARCHE = 12743; // registre des cde Marches bit à bit
 //constexpr uint16_t START_REG_BP_ARRET = 12744; // registre des cde Arret bit à bit
 
-constexpr uint16_t BP_MARCHE_CHAUD = 19568; // Bobine cde Marche
-constexpr uint16_t BP_ARRET_CHAUD = 19584; // Bobine cde Arret
+constexpr uint16_t BP_MARCHE_CHAUD = 19568;     // %MX455.0  Bobine cde Marche
+constexpr uint16_t BP_ARRET_CHAUD = 19584;      // %MX457.0 Bobine cde Arret
 
-constexpr uint16_t BP_MARCHE_BOOST = 19573; // Bobine cde Marche
-constexpr uint16_t BP_ARRET_BOOST = 19589; // Bobine cde Arret
+constexpr uint16_t BP_MARCHE_BOOST = 19573;     // %MX455.5 Bobine cde Marche
+constexpr uint16_t BP_ARRET_BOOST = 19589;      // %MX457.5 Bobine cde Arret
 
-constexpr uint16_t BP_MARCHE_RADIAT = 19569; // Bobine cde Marche
-constexpr uint16_t BP_ARRET_RADIAT = 19585; // Bobine cde Arret
+constexpr uint16_t BP_MARCHE_RADIAT = 19569;    // %MX455.1  Bobine cde Marche
+constexpr uint16_t BP_ARRET_RADIAT = 19585;     // %MX457.1 Bobine cde Arret
 
-constexpr uint16_t BP_MARCHE_PLANCHER = 19570; // Bobine cde Marche
-constexpr uint16_t BP_ARRET_PLANCHER = 19586; // Bobine cde Arret
+constexpr uint16_t BP_MARCHE_PLANCHER = 19570;  // %MX455.2 Bobine cde Marche
+constexpr uint16_t BP_ARRET_PLANCHER = 19586;   // %MX457.2 Bobine cde Arret
 
-constexpr uint16_t START_REG_ANIM = 12745;       // Retour etat pour animation pompes, ch. lampes, etc 
-constexpr uint16_t NB_REGS_ANIM = 6;           // Number of holding registers to read  y compris les Alarmes
+constexpr uint16_t START_REG_ANIM = 12745;      // Retour etat pour animation pompes, ch. lampes, etc 
+constexpr uint16_t NB_REGS_ANIM = 6;            // Number of holding registers to read  y compris les Alarmes
 
-constexpr uint16_t BP_LAMPE_SSOL = 19488;    // BP Marche lampe sous sol
-constexpr uint16_t BP_LAMPE_RDC = 19489;     // BP Marche lampe RdC
-constexpr uint16_t BP_LAMPE_1ETAGE = 19490;  // BP Marche lampe 1erEtage
+constexpr uint16_t BP_LAMPE_SSOL = 19488;       // BP Marche lampe sous sol
+constexpr uint16_t BP_LAMPE_RDC = 19489;        // BP Marche lampe RdC
+constexpr uint16_t BP_LAMPE_1ETAGE = 19490;     // BP Marche lampe 1erEtage
 
-constexpr uint16_t BP_OUVERT_GARAGE = 19491;  // BP ouverture porte de garage
-constexpr uint16_t PORTE_GARAGE1_FERME = 19492;  // Etat porte de garage fermé
+constexpr uint16_t BP_OUVERT_GARAGE = 19491;    // BP ouverture porte de garage
+constexpr uint16_t PORTE_GARAGE1_FERME = 19492; // Etat porte de garage fermé
 
 //Lecture des Bits de Sortie 
 // de "000512   , 16 ); 
